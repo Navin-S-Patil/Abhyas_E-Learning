485abe729a7592448ae20db8fe316f2047544297
Navin Patil <navinpatilwork@gmail.com>
2022-07-17 21:33:13 +0530

    make it better

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.use('/api/private', require('./routes/private'));
 //Error Handler (Should be last pieace of middleware)
 app.use(errorHandler);
 
+//server production assests
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static("client/build"));
+  // const path = require("path");
+
+    // app.get("*", (req, res) => {
+
+    //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+
+    // })
+}
+
 
 const PORT = process.env.PORT || 5000;
 
